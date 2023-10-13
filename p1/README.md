729c3feb3dee470fd40502373d8b90df67c9de11
Noe Dormoy <noedormoy@macbook-pro-de-noe-2.home>
2023-10-13 19:11:28 +0200

    k3s not working

--- p1/README.md
+++ p1/README.md
@@ -14,24 +14,13 @@ Commandes Vagrant :
 Doc Valgrant :
 https://developer.hashicorp.com/vagrant/docs/networking/private_network
 
-On utilise la box centos 8, qui va etre notre systeme d'exploitation pour les machines virtuelles.
-
-https://app.vagrantup.com/generic/boxes/centos8
-```
-Vagrant.configure("2") do |config|
-  config.vm.box = "centos/8"
-end
-```
-
-export VAGRANT_HOME=/path/to/external/disk/vagrant
-default : ~/.vagrant.d
-
 Pour se connecter en SSH :
 vagrant ssh ndormoyS
 vagrant ssh ndormoySW
 
-Pour regler pb shared file ?
-vagrant plugin install vagrant-vbguest --> marche pas
+
+k3s doc:
+https://docs.k3s.io/installation/network-options
 
 
 Ce qu'il faut tester :
--- p1/elements/install_k3s_server.sh
+++ p1/elements/install_k3s_server.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-# # Fix the problem of Failed to download metadata for repo 'appstream'
-# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# Fix the problem of Failed to download metadata for repo 'appstream'
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-# # Install K3s in controller mode
+# Install K3s in controller mode
 # curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -
 
-# # # Capture the server node token from /var/lib/rancher/k3s/server/node-token
-# # token=$(cat /var/lib/rancher/k3s/server/node-token)
+# # Set the desired server node IP address and install K3s in server mode
+SERVER_IP="192.168.56.110"
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=${SERVER_IP}" sh -
 
-# # # Save the token to a file
-# # echo "$token" > /vagrant/server_node_token
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode '0644' --kubelet-arg 'config=/etc/rancher/k3s/kubelet.config' --node-ip ${SERVER_IP}" sh -
 
+
+/usr/local/bin/k3s server --write-kubeconfig-mode 0644 --kubelet-arg "config=/etc/rancher/k3s/kubelet.config" --node-ip="${SERVER_IP}"
 
 # sleep 10
 
-# # ls -l /var/lib/rancher/k3s/server/
+# # Create directory for the Token
 # mkdir /vagrant/server_node_token
+mkdir -p /vagrant/server_node_token
 
-# cp /var/lib/rancher/k3s/server/node-token /vagrant/server_node_token/
-# chmod 755 -R /vagrant/server_node_token 
+# Copy the token to the shared directory
+cp /var/lib/rancher/k3s/server/node-token /vagrant/server_node_token/
+
+# Ensure the good persimissions for the server node token file
+chmod 755 -R /vagrant/server_node_token
+
+# Ensure the server kubernetes file has correct permissions
+chmod 755 /etc/rancher/k3s/k3s.yaml
