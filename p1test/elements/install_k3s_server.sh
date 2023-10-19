50e6c538b3cbd2a0482899ffdd339741a4670bc4
Noe Dormoy <noedormoy@macbook-pro-de-noe.home>
2023-10-19 13:58:12 +0200

    Working, much cleaner, add real waiting in bash script to wait k3s start

---
+++ p1test/elements/install_k3s_server.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo -i
+apk add curl
+
+rm -rf /vagrant/server_node_token
+mkdir -p /vagrant/server_node_token
+
+# export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=#{S_IP} --node-external-ip=#{S_IP} --flannel-iface=eth1"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=$1 --flannel-iface=eth1"
+
+
+echo "INSTALL_K3S_EXEC : ${INSTALL_K3S_EXEC}"
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+echo "KUBECONFIG : ${KUBECONFIG}"
+
+curl -sfL https://get.k3s.io | sh -
+
+# Use a while loop to check if K3s is running
+while :; do
+    if /usr/local/bin/k3s kubectl get nodes 2>/dev/null | grep -q " Ready "; then
+        echo "INFO: k3s is ready."
+        break
+    else
+        echo "INFO: Waiting for k3s to start up..."
+        sleep 3
+    fi
+done
+
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/server_node_token/
+echo "/var/lib/rancher/k3s/server/node-token : $(cat /var/lib/rancher/k3s/server/node-token)"
+chmod 755 -R /vagrant/server_node_token/
+
+chmod 755 /etc/rancher/k3s/k3s.yaml
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/server_node_token/
+
+echo "/etc/rancher/k3s/k3s.yaml : $(cat /etc/rancher/k3s/k3s.yaml)"
---
+++ p1test/elements/install_k3s_worker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo -i
+apk add curl
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+echo "KUBECONFIG : ${KUBECONFIG}"
+sudo cp /vagrant/server_node_token/k3s.yaml /etc/rancher/k3s/k3s.yaml
+
+echo "/etc/rancher/k3s/k3s.yaml : $(cat /etc/rancher/k3s/k3s.yaml)"
+export K3S_TOKEN_FILE=/vagrant/server_node_token/node-token
+
+echo "K3S_TOKEN_FILE : ${K3S_TOKEN_FILE}"
+export K3S_URL=https://$1:6443
+echo "K3S_URL : ${K3S_URL}"
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+echo "INSTALL_K3S_EXEC : ${INSTALL_K3S_EXEC}"
+curl -sfL https://get.k3s.io | sh -
